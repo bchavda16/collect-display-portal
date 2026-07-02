44c5f848605c1458c998506a49f17cebef5c1726
Bhavik Chavda <bhavikchavda@Mac.lan>
2026-07-02 14:12:25 +0100

    add trustHost for netlify

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,6 +18,7 @@ const loginSchema = z.object({
 export const { handlers, auth, signIn, signOut } = NextAuth({
   
   session: { strategy: "jwt" },
+  trustHost: true,
   pages: {
     signIn: "/login",
     error: "/login",
